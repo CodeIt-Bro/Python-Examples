14493604f5ce9215394dde165c5c5b042648135d
CodeItBro <34979541+CodeIt-Bro@users.noreply.github.com>
2020-01-11 18:04:53 +0530

    Add files via upload

---
+++ Add Two Numbers.py
@@ -0,0 +1,15 @@
+# Store input numbers
+firstNumber = input('Enter first number: ')
+secondNumber = input('Enter second number: ')
+# Add two numbers
+addition = float(firstNumber) + float(secondNumber)
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(firstNumber, secondNumber, addition))
+
+# Store input numbers
+#firstNumber = 3.4
+#secondNumber = 4.5
+# Add two numbers
+#addition = float(firstNumber) + float(secondNumber)
+# Display the sum
+#print('The sum of {0} and {1} is {2}'.format(firstNumber, secondNumber, addition))
