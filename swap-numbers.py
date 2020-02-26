997002cbab575cf2e8a9b3f5752d48b30116e1e8
CodeItBro <34979541+CodeIt-Bro@users.noreply.github.com>
2020-02-26 23:07:00 +0530

    Add files via upload

---
+++ swap-numbers.py
@@ -0,0 +1,19 @@
+firstNum = input("Enter first number = ")
+secondNum = input("Enter second number = ")
+print("First Number = ", firstNum)
+print("Second Number = ", secondNum)
+
+#code to swap two numbers using a third variable
+#print("After swapping using a temp variable")
+#xyz = firstNum
+#firstNum = secondNum
+#secondNum = xyz
+#print("First Number Now = ", firstNum)
+#print("Second Number Now = ", secondNum)
+
+#Code to swap two numbers without any third variable
+
+firstNum, secondNum = secondNum, firstNum
+print("After swapping without using a temp variable")
+print("First Number Now = ", firstNum)
+print("Second Number Now = ", secondNum)
