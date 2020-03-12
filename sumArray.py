6a673d05b995ce62b3d1d7d47f2364b396baf38e
CodeItBro <34979541+CodeIt-Bro@users.noreply.github.com>
2020-03-13 00:19:56 +0530

    Add files via upload
    Python Program To Find Sum of Array

---
+++ sumArray.py
@@ -0,0 +1,6 @@
+Age = [10, 12, 13]       #Inputting Array values. 
+theSum = 0               #Definig variable to store the sum of array. 
+for i in Age:            #For loop to iterate through the array
+ theSum = theSum + i     #Adding array elements. 
+
+print(theSum)            #Outputting the sum of array elements. 
